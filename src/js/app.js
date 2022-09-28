e4d15eeb8333d288f1a8b6e9e322942f97108b64
Zenon Marek Raubuć <m.raubuc@wp.pl>
2022-09-28 21:13:24 +0200

    Update project Cal

--- src/js/app.js
+++ src/js/app.js
@@ -1,54 +1,67 @@
-let buttons = document.querySelectorAll(".btnClass");
-let dispControl = document.querySelector("#resDisplay");
+const buttons = document.querySelectorAll('.btnClass');
+const displayControl = document.querySelector('#resDisplay');
 let prevValue = null;
 let lastOperator = null;
-let currValue = "";
+let currValue = '';
 
 let refreshDisplay = () => {
-    //console.log(currValue);
-    if (currValue.length > 0) dispControl.textContent = currValue;
-    else if (prevValue !== null) dispControl.textContent = prevValue;
-    else dispControl.textContent = "0";
+  //console.log(currValue);
+  if (currValue.length > 0) {
+    displayControl.textContent = currValue;
+  } else if (prevValue !== null) {
+    displayControl.textContent = prevValue;
+  } else {
+    displayControl.textContent = '0';
+  }
 };
 
 let calculate = (val1, val2, operator) => {
-    let v1Num = Number(val1);
-    let v2Num = Number(val2);
-    if (operator === "+") return v1Num + v2Num;
-    if (operator === "-") return v1Num - v2Num;
-    if (operator === "*") return v1Num * v2Num;
-    if (operator === "/") return v1Num / v2Num;
+  let v1Num = Number(val1);
+  let v2Num = Number(val2);
+
+  if (operator === '+') return v1Num + v2Num;
+  if (operator === '-') return v1Num - v2Num;
+  if (operator === '*') return v1Num * v2Num;
+  if (operator === '/') return v1Num / v2Num;
 };
 
-let onBtnClick = (e) => {
-    let btnId = e.target.id;
-    if (btnId.startsWith("btn")) {
-        currValue = currValue + btnId.replace("btn", ""); // number button. Add to currValue string
-    } else if (btnId === "comma") {
-        if (currValue === "") currValue = "0"; // if no number in currValue, put zero
-        currValue = currValue + ".";
-    } else if (btnId.startsWith("oper")) {
-        let operator = btnId.replace("oper", ""); // get operator from id
-        if (prevValue === null) {
-            prevValue = currValue; // first number. Do nothing except write number to last value variable
-        } else {
-            prevValue = calculate(prevValue, currValue, lastOperator); // calculate and store in last value variable
-        }
-        lastOperator = operator; // store operator and reset current value
-        currValue = "";
-    } else if (btnId.startsWith("result")) {
-        currValue = calculate(prevValue, currValue, lastOperator).toString(); // calclulate and reset last value
-        prevValue = null;
+const onBtnClick = e => {
+  let btnId = e.target.id;
+
+  if (btnId.startsWith('btn')) {
+    // number button. Add to currValue string
+    currValue = currValue + btnId.replace('btn', '');
+  } else if (btnId === 'comma') {
+    // if no number in currValue, put zero
+    if (currValue === '') currValue = '0';
+    currValue = currValue + '.';
+  } else if (btnId.startsWith('oper')) {
+    // get operator from id
+    let operator = btnId.replace('oper', '');
+    if (prevValue === null) {
+      // first number. Do nothing except write number to last value variable
+      prevValue = currValue;
+    } else {
+      // calculate and store in last value variable
+      prevValue = calculate(prevValue, currValue, lastOperator);
     }
+    // store operator and reset current value
+    lastOperator = operator;
+    currValue = '';
+  } else if (btnId.startsWith('result')) {
+    // calculate and reset last value
+    currValue = calculate(prevValue, currValue, lastOperator).toString();
+    prevValue = null;
+  }
 
-    refreshDisplay();
+  refreshDisplay();
 };
 
-let addListeners = () => {
-    let btnArray = [...buttons];
-    btnArray.forEach((btn) => {
-        btn.addEventListener("click", onBtnClick);
-    });
+const addListeners = () => {
+  let btnArray = [...buttons];
+  btnArray.forEach(btn => {
+    btn.addEventListener('click', onBtnClick);
+  });
 };
 
 addListeners();
